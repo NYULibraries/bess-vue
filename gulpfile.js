706a3eddfaa74cc5b614085482af6f5f5caf9acd
Barnaby Alter <barnaby.alter@nyu.edu>
2017-10-06 16:59:26 -0400

    Setup gulp tasks to browserify, babelify, uglify JS and compile, minify CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,45 @@
-var gulp = require('gulp');
+// App name, used in naming the dist files
+let appName = 'primo_search_embed';
+
+// Dependencies
+let gulp = require('gulp');
+let browserify = require('browserify');
+let source = require('vinyl-source-stream');
+let buffer = require('vinyl-buffer');
+let gutil = require('gulp-util');
+let uglify = require('gulp-uglify');
+let sourcemaps = require('gulp-sourcemaps');
+let sass = require('gulp-sass');
+let cleanCss = require('gulp-clean-css');
+let concat = require('gulp-concat');
+
+// Browserify, uglify and babelify
+//    usage: gulp js
+gulp.task('js', function() {
+  const jsFilename = appName + '.js';
+  // Set up browserify
+  const browserified = browserify({
+    entries: 'js/' + jsFilename,
+    debug: true,
+  }).transform("babelify", {presets: ["es2015"]}).bundle();
+
+  // Start piping
+  return browserified
+    .pipe(source(jsFilename))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+        .pipe(uglify())
+        .on('error', gutil.log)
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./dist'));
+});
+
+// Compile form SCSS and minify
+//    usage: gulp css --vid NYU
+gulp.task('css', function() {
+  return gulp.src(['sass/**/*.scss'])
+    .pipe(sass())
+    .pipe(cleanCss())
+    .pipe(concat(gutil.env.vid.toLowerCase() + '-min.css'))
+    .pipe(gulp.dest('./dist'))
+});
---
+++ js/primo_search_embed.js
@@ -0,0 +1,5 @@
+(() => {
+  const sfx = require('./sfx');
+  // Doing all my stuff here
+  console.log('test');
+})();
