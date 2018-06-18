d8c313326ff5dd11f0a2931fd7a9dc1fb74e421d
Elliott Grieco <elliott.grieco@gmail.com>
2018-06-18 18:16:27 -0400

    Gets target element from query string

--- js/index.js
+++ js/index.js
@@ -1,7 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
+import qs from 'query-string';
+
+const queryString = document.currentScript.src.replace(/^[^\?]+\??/,'');
+const { element_id: id } = qs.parse(queryString);
 
 const app = new Vue({
-  el: '#primo_explore_search_embed_container',
+  el: `#${id}`,
   render: h => h(App)
 });
