d37239881279263c70689abb0cc69300b16915ac
Elliott Grieco <elliott.grieco@nyu.edu>
2019-01-28 11:29:14 -0500

    Vastly simplifies environments structure

--- config.js
+++ config.js
@@ -1,43 +1,46 @@
 const yaml = require('yaml');
 const fs = require('fs');
-const { render } = require('mustache');
-const R = require('ramda');
+
+// piping utility function
+const _pipe = (a, b) => (arg) => b(a(arg));
+const pipe = (...ops) => ops.reduce(_pipe);
+// mustache utlity function
+const mustache = (string, data = {}) =>
+  Object.entries(data).reduce((res, [key, value]) => res.replace(new RegExp(`{{\\s*${key}\\s*}}`, "g"), value), string);
 
 const parseYaml = str => yaml.parse(str, { merge: true });
 const readFile = path => fs.readFileSync(path, 'utf8');
-const parseYamlFromPath = path => parseYaml(readFile(path));
-const parseYamlIfPathWithDefault = defaultVal => path => path === undefined ? defaultVal : parseYamlFromPath(path);
 
-// curryable Mustache.render, expects only 2 arguments
-const renderMustache = R.curryN(2, render);
-
-// gets mustache yml file path based on schema: { environments: { staging: { mustache: './myMustacheValues.yml' } } }
-// if not found, returns undefined and warns
-// defaults to NODE_ENV: development
-const getMustacheYamlPath = config => {
+// gets variables to mustache based on schema: { environments: { staging: { var1: val1, var2: val2, ... } } }
+// if not found, returns {} and warns
+const getMustacheHash = config => {
   const warn = () => console.warn(
-    'A mustache environment has not been set.',
     '{{ Mustached }} values will not be processed.'
   );
+
   try {
-    const path = config.environments[process.env.NODE_ENV || 'development'].mustache;
-    return path === undefined ? warn() : path;
+    const hash = config.environments[process.env.NODE_ENV];
+    if (hash === undefined) {
+      warn();
+    }
+    return hash || {};
   } catch(err) {
     warn();
+    return {}
   }
 };
 
-const configYaml = readFile(process.env.CONFIG || './config.yml');
-const config = R.pipe(
+
+const configYamlTemplate = readFile(process.env.CONFIG || './config.yml');
+const config = pipe(
   parseYaml,
-  getMustacheYamlPath,
-  parseYamlIfPathWithDefault({}),
-  renderMustache(configYaml),
+  getMustacheHash,
+  mustache.bind(null, configYamlTemplate),
   parseYaml
-)(configYaml);
+)(configYamlTemplate);
 
 /* Uncomment to inspect rendered config: */
-// const { inspect } = require('util');
-// console.log(inspect(config, { showHidden: false, depth: null }));
+const { inspect } = require('util');
+console.log(inspect(config, { showHidden: false, depth: null }));
 
 module.exports = config;
