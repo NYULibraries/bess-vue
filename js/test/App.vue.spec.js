2c56e67032670dea3c39b64c6ae3617ea069a2ae
Elliott Grieco <elliott.grieco@nyu.edu>
2019-01-28 15:53:56 -0500

    App test cleanup

--- js/test/App.vue.spec.js
+++ js/test/App.vue.spec.js
@@ -248,12 +248,6 @@ describe('App', () => {
   });
 
   describe('shallow render', () => {
-    let tabWrappers;
-
-    beforeEach(() => {
-      tabWrappers = wrapper.findAll('div.bobcat_embed_tabs ul tab-item-stub').wrappers;
-    })
-
     it('has a list of tabs', () => {
       expect(wrapper.contains('div.bobcat_embed_tabs ul'));
     });
@@ -272,155 +266,161 @@ describe('App', () => {
       });
     });
 
-
-    it('has an appropriate number of tabs', () => {
-      expect(tabWrappers.length).toEqual(2);
-    });
-
-    describe('initial state', () => {
-      describe('the first tab', () => {
-        const tabProps = config.tabs.one;
-        let firstTab;
-        beforeEach(() => {
-          firstTab = tabWrappers[0];
-        });
-
-        it('is selected by default', () => {
-          expect(firstTab.classes('bobcat_embed_tabs_selected')).toBe(true);
-        });
-
-        it('has the appropriate "first" class', () => {
-          expect(firstTab.classes('bobcat_embed_tabs_first')).toBe(true);
-        });
-
-        it('does not have inner, last classes', () => {
-          expect(firstTab.classes('bobcat_embed_tabs_inner')).toBe(false);
-          expect(firstTab.classes('bobcat_embed_tabs_last')).toBe(false);
-        });
-
-        it('has a tab with the appropriate properties', () => {
-          const vm = firstTab.vm;
-
-          expect(vm.tab).toBeDefined();
-          Object.keys(tabProps).forEach(prop => {
-            expect(vm.tab[prop]).toEqual(tabProps[prop]);
-          });
-        });
-
-        it('has a selected attribute', () => {
-          expect(firstTab.vm.selected).toBeDefined();
-        });
-
-        it('is selected by default', () => {
-          expect(firstTab.vm.selected).toBe(true);
-        });
-
-        it('has an update-tab function', () => {
-          expect(firstTab.vm.updateTab).toBeDefined();
-          expect(typeof firstTab.vm.updateTab).toEqual('function');
-        });
-
-        it('has an attribute role=tab', () => {
-          expect(firstTab.attributes('role')).toEqual('tab');
-        });
-      });
-
-      describe('the second tab', () => {
-        const tabProps = config.tabs.two;
-        let secondTab;
-        beforeEach(() => {
-          secondTab = tabWrappers[1];
-        });
-
-        it('is not selected by default', () => {
-          expect(secondTab.classes('bobcat_embed_tabs_selected')).toBe(false);
-        });
-
-        it('has the appropriate "last" class', () => {
-          expect(secondTab.classes('bobcat_embed_tabs_last')).toBe(true);
-        });
-
-        it('does not have inner, first classes', () => {
-          expect(secondTab.classes('bobcat_embed_tabs_inner')).toBe(false);
-          expect(secondTab.classes('bobcat_embed_tabs_first')).toBe(false);
-        });
-
-        it('has a tab with the appropriate properties', () => {
-          const vm = secondTab.vm;
-
-          Object.keys(tabProps).forEach(prop => {
-            expect(vm.tab[prop]).toEqual(tabProps[prop]);
-          });
-        });
-
-        it('has a selected attribute', () => {
-          expect(secondTab.vm.selected).toBeDefined();
-        });
-
-        it('is selected by default', () => {
-          expect(secondTab.vm.selected).toBe(false);
-        });
-
-        it('has an update-tab function', () => {
-          expect(secondTab.vm.updateTab).toBeDefined();
-          expect(typeof secondTab.vm.updateTab).toEqual('function');
-        });
-
-        it('has an attribute role=tab', () => {
-          expect(secondTab.attributes('role')).toEqual('tab');
-        });
-      });
-    });
-
-    describe('when second tab selected', () => {
+    describe('<tab-item>', () => {
+      let tabWrappers;
       beforeEach(() => {
-        wrapper.setData({
-          selectedTab: 'two',
+        tabWrappers = wrapper.findAll('div.bobcat_embed_tabs ul tab-item-stub').wrappers;
+      });
+
+       it('has an appropriate number of tabs', () => {
+        expect(tabWrappers.length).toEqual(2);
+      });
+
+      describe('initial state', () => {
+        describe('the first tab', () => {
+          const tabProps = config.tabs.one;
+          let firstTab;
+          beforeEach(() => {
+            firstTab = tabWrappers[0];
+          });
+
+          it('is selected by default', () => {
+            expect(firstTab.classes('bobcat_embed_tabs_selected')).toBe(true);
+          });
+
+          it('has the appropriate "first" class', () => {
+            expect(firstTab.classes('bobcat_embed_tabs_first')).toBe(true);
+          });
+
+          it('does not have inner, last classes', () => {
+            expect(firstTab.classes('bobcat_embed_tabs_inner')).toBe(false);
+            expect(firstTab.classes('bobcat_embed_tabs_last')).toBe(false);
+          });
+
+          it('has a tab with the appropriate properties', () => {
+            const vm = firstTab.vm;
+
+            expect(vm.tab).toBeDefined();
+            Object.keys(tabProps).forEach(prop => {
+              expect(vm.tab[prop]).toEqual(tabProps[prop]);
+            });
+          });
+
+          it('has a selected attribute', () => {
+            expect(firstTab.vm.selected).toBeDefined();
+          });
+
+          it('is selected by default', () => {
+            expect(firstTab.vm.selected).toBe(true);
+          });
+
+          it('has an update-tab function', () => {
+            expect(firstTab.vm.updateTab).toBeDefined();
+            expect(typeof firstTab.vm.updateTab).toEqual('function');
+          });
+
+          it('has an attribute role=tab', () => {
+            expect(firstTab.attributes('role')).toEqual('tab');
+          });
+        });
+
+        describe('the second tab', () => {
+          const tabProps = config.tabs.two;
+          let secondTab;
+          beforeEach(() => {
+            secondTab = tabWrappers[1];
+          });
+
+          it('is not selected by default', () => {
+            expect(secondTab.classes('bobcat_embed_tabs_selected')).toBe(false);
+          });
+
+          it('has the appropriate "last" class', () => {
+            expect(secondTab.classes('bobcat_embed_tabs_last')).toBe(true);
+          });
+
+          it('does not have inner, first classes', () => {
+            expect(secondTab.classes('bobcat_embed_tabs_inner')).toBe(false);
+            expect(secondTab.classes('bobcat_embed_tabs_first')).toBe(false);
+          });
+
+          it('has a tab with the appropriate properties', () => {
+            const vm = secondTab.vm;
+
+            Object.keys(tabProps).forEach(prop => {
+              expect(vm.tab[prop]).toEqual(tabProps[prop]);
+            });
+          });
+
+          it('has a selected attribute', () => {
+            expect(secondTab.vm.selected).toBeDefined();
+          });
+
+          it('is selected by default', () => {
+            expect(secondTab.vm.selected).toBe(false);
+          });
+
+          it('has an update-tab function', () => {
+            expect(secondTab.vm.updateTab).toBeDefined();
+            expect(typeof secondTab.vm.updateTab).toEqual('function');
+          });
+
+          it('has an attribute role=tab', () => {
+            expect(secondTab.attributes('role')).toEqual('tab');
+          });
         });
       });
 
-      describe('the first tab', () => {
-        let firstTab;
+      describe('when second tab selected', () => {
         beforeEach(() => {
-          firstTab = tabWrappers[0];
+          wrapper.setData({
+            selectedTab: 'two',
+          });
         });
 
-        it(`does not have the 'selected' class`, () => {
-          expect(firstTab.classes('bobcat_embed_tabs_selected')).toBe(false);
+        describe('the first tab', () => {
+          let firstTab;
+          beforeEach(() => {
+            firstTab = tabWrappers[0];
+          });
+
+          it(`does not have the 'selected' class`, () => {
+            expect(firstTab.classes('bobcat_embed_tabs_selected')).toBe(false);
+          });
+
+          it('selected attribute is false', () => {
+            expect(firstTab.vm.selected).toBe(false);
+          });
         });
 
-        it('selected attribute is false', () => {
-          expect(firstTab.vm.selected).toBe(false);
+        describe('the second tab', () => {
+          let secondTab;
+          beforeEach(() => {
+            secondTab = tabWrappers[1];
+          });
+
+          it(`has the 'selected' class`, () => {
+            expect(secondTab.classes('bobcat_embed_tabs_selected')).toBe(true);
+          });
+
+          it('selected attribute is true', () => {
+            expect(secondTab.vm.selected).toBe(true);
+          });
         });
-      });
 
-      describe('the second tab', () => {
-        let secondTab;
-        beforeEach(() => {
-          secondTab = tabWrappers[1];
-        });
+        describe('links list', () => {
+          it(`lists the second tab's advanced links`, () => {
+            const linksWrapper = wrapper.find('.bobcat_embed_links');
+            const links = linksWrapper.findAll('.bobcat_embed_links ul li a').wrappers;
+            const expectedLinks = config.tabLinks.two;
 
-        it(`has the 'selected' class`, () => {
-          expect(secondTab.classes('bobcat_embed_tabs_selected')).toBe(true);
-        });
+            expect(links.length).toEqual(expectedLinks.length);
 
-        it('selected attribute is true', () => {
-          expect(secondTab.vm.selected).toBe(true);
-        });
-      });
-
-      describe('links list', () => {
-        it(`lists the second tab's advanced links`, () => {
-          const linksWrapper = wrapper.find('.bobcat_embed_links');
-          const links = linksWrapper.findAll('.bobcat_embed_links ul li a').wrappers;
-          const expectedLinks = config.tabLinks.two;
-
-          expect(links.length).toEqual(expectedLinks.length);
-
-          links.forEach((link, idx) => {
-            expect(link.attributes('href')).toEqual(expectedLinks[idx].href);
-            expect(link.text()).toEqual(expectedLinks[idx].label);
-            expect(link.attributes('target')).toEqual('_blank');
+            links.forEach((link, idx) => {
+              expect(link.attributes('href')).toEqual(expectedLinks[idx].href);
+              expect(link.text()).toEqual(expectedLinks[idx].label);
+              expect(link.attributes('target')).toEqual('_blank');
+            });
           });
         });
       });
