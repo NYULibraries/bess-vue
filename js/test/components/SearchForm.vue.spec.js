7a2ada080159deb1658bb7e2ef6e9b84cd483ad5
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2024-03-14 11:35:58 -0400

    SearchForm.vue.spec.js: Update test to reflect the UX changes
    Signed-off-by: Vlad Burlutskiy <vlad.burlutskiy@icloud.com>

--- js/test/components/SearchForm.vue.spec.js
+++ js/test/components/SearchForm.vue.spec.js
@@ -58,7 +58,7 @@ describe('SearchForm', () => {
       });
       it(`has appropriate label for 'guides' engine`, async () => {
         await wrapper.setProps({ engine: { type: 'guides' } });
-        expect(wrapper.vm.inputAriaLabel).toBe(`Search for subject guides`);
+        expect(wrapper.vm.inputAriaLabel).toBe(`Search for research guides`);
       });
     });
   });
