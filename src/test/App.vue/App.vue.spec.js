311f04ed5be970ee0567455e811387b8fe60c932
David <da70@nyu.edu>
2024-09-10 15:44:57 -0400

    App.vue.spec.js: add tests for search scope dropdowns to "engine search > should call `window.open` with correct URL" test suite

--- src/test/App.vue/App.vue.spec.js
+++ src/test/App.vue/App.vue.spec.js
@@ -80,6 +80,21 @@ describe( `App [ VITE_DEPLOY_ENV: ${ process.env.VITE_DEPLOY_ENV } ]`, () => {
                 },
             };
 
+            // This is used for the `describe.runIf` test conditions below.
+            // Originally tried to do something like this:
+            //
+            //    describe.runIf( appConfig.institutions[ institution ][ 0 ].ui?.searchScopeDropdown )( ...
+            //        describe.each( [...appConfig.institutions[ institution ][ 0 ].ui.searchScopeDropdown.options.map( option => { searchScopeDropdownValue: option } )( ...
+            //
+            // ...but it looks like the `describe.each` test is evaluated whether
+            // the `describe.runIf` condition is truthy or not, leading to errors
+            // when trying to read the `searchScopeDropdown` property of an
+            // undefined `appConfig.institutions[ institution ][ 0 ].ui`.
+            let currentConfig = appConfig.institutions[ institution ];
+            let searchScopeDropdownOptionValues = currentConfig[ 0 ].ui?.searchScopeDropdown.options ?
+                Object.keys( currentConfig[ 0 ].ui?.searchScopeDropdown.options ) :
+                [];
+
             beforeEach( () => {
                 // Suppress error "Error: Not implemented: window.open", which doesn't
                 // cause the test to fail but does make test output harder to read.
@@ -97,22 +112,52 @@ describe( `App [ VITE_DEPLOY_ENV: ${ process.env.VITE_DEPLOY_ENV } ]`, () => {
                 testState.reset();
             } );
 
-            describe( 'should call `window.open` with correct URL', () => {
+            describe.runIf( searchScopeDropdownOptionValues.length === 0 )( 'should call `window.open` with correct URL', () => {
                 test.each(
                     [
-                        { title: 'empty search', value: EMPTY_SEARCH },
-                        { title: 'all-whitespace search', value: ALL_WHITESPACE_SEARCH },
-                        { title: 'non-empty search', value: NON_EMPTY_SEARCH },
-                    ] )( '$title: $value', async ( { value } ) => {
+                        { title: 'empty search', inputValue: EMPTY_SEARCH },
+                        { title: 'all-whitespace search', inputValue: ALL_WHITESPACE_SEARCH },
+                        { title: 'non-empty search', inputValue: NON_EMPTY_SEARCH },
+                    ] )( '$title: $inputValue', async ( { inputValue } ) => {
                     expect( testState.target ).toBeUndefined();
 
                     const form = wrapper.find( 'form' )
                     const input = form.find( 'input' );
-                    await input.setValue( value );
+                    await input.setValue( inputValue );
                     form.trigger( 'submit' );
 
                     expect( testState.target ).toMatchSnapshot();
                 } )
+            } );
+
+            describe.runIf( searchScopeDropdownOptionValues.length > 0 )( 'should call `window.open` with correct URL', () => {
+                describe.each(
+                    [
+                        ...searchScopeDropdownOptionValues.map( function ( option ) {
+                            return { 'searchScopeDropdownValue': option }
+                        } ),
+                    ],
+                )(
+                    'for search scope dropdown value: $searchScopeDropdownValue', ( { searchScopeDropdownValue } ) => {
+                        test.each(
+                            [
+                                { title: 'empty search', inputValue: EMPTY_SEARCH },
+                                { title: 'all-whitespace search', inputValue: ALL_WHITESPACE_SEARCH },
+                                { title: 'non-empty search', inputValue: NON_EMPTY_SEARCH },
+                            ] )( '$title: $inputValue', async ( { inputValue } ) => {
+                            expect( testState.target ).toBeUndefined();
+
+                            const form = wrapper.find( 'form' )
+                            const searchScopeDropdown = form.find( 'select' );
+                            const input = form.find( 'input' );
+                            await searchScopeDropdown.setValue( searchScopeDropdownValue );
+                            await input.setValue( inputValue );
+                            form.trigger( 'submit' );
+
+                            expect( testState.target ).toMatchSnapshot();
+                        } )
+                    },
+                );
             } );
         } );
     } );
