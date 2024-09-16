86f88481b3ab866b17ee18b2ce7b5a3523d5a898
David <da70@nyu.edu>
2024-09-16 15:54:36 -0400

    config/institutions/nyush.js: initial commit of `ui` with `searchScopeDropdown` stuff

--- config/institutions/nyush.js
+++ config/institutions/nyush.js
@@ -19,6 +19,40 @@ export default [
             `<a href="https://search.shanghai.library.nyu.edu/discovery/search?vid=${ vid }&mode=advanced" target="_blank">Advanced search</a>`,
             `Need the full text of an article? <a href="https://search.shanghai.library.nyu.edu/discovery/citationlinker?vid=${ vid }" target="_blank">Use the search by citation tool</a>.`,
         ],
+        ui: {
+            // Source: search scopes dropdown in Primo
+            // xpath query as of 2024-08-15: /html/body/primo-explore/div/prm-explore-main/div/prm-search-bar/div[1]/div/div[2]/div/form/div/div/div[2]/prm-tabs-and-scopes-selector/div/md-input-container/md-select/div/md-select-menu/md-content/div[2]
+            searchScopeDropdown: {
+                defaultOption: 'CI_NYUSH',
+                options      : {
+                    CI_NYUSH: {
+                        label      : 'All at Shanghai',
+                        placeholder: '',
+                    },
+                    CI_NYUSH_NYU_CONSORTIA: {
+                        label      : 'All at NYU',
+                        placeholder: '',
+                    },
+                    // Do not change this to CI_ARTICLES.  The search scope query
+                    // parameter is apparently case-sensitive.  Compare results:
+                    //
+                    // * "CI_articles": https://search.shanghai.library.nyu.edu/discovery/search?query=any,contains,art&tab=default_slot&search_scope=CI_articles&vid=01NYU_US:SH&offset=0
+                    // * "CI_ARTICLES": https://search.shanghai.library.nyu.edu/discovery/search?query=any,contains,art&tab=default_slot&search_scope=CI_ARTICLES&vid=01NYU_US:SH&offset=0
+                    //
+                    // As of 2024-09-16, the CI_ARTICLES URL redirects to:
+                    // https://search.shanghai.library.nyu.edu/discovery/search?query=any,contains,art&tab=default_slot&vid=01NYU_US:SH&offset=0
+                    // ...`search_scope` query param is removed.
+                    CI_articles: {
+                        label      : 'All Articles',
+                        placeholder: '',
+                    },
+                    NYUSH: {
+                        label      : 'Classic Search',
+                        placeholder: '',
+                    },
+                },
+            },
+        },
     },
     shared.tabs.guidesArticles,
     shared.tabs.aresReserves,
